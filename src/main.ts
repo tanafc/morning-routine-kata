8e0494d9746c0f9ed0fbcf2d6f4c4f96ee29af6e
DiegoAcidTango <diegoperez@acidtango.com>
2024-03-08 15:48:14 +0000

    green: returns do exercise in between 06:00 and 06:59

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ interface IMorningRoutine {
 
 export class MorningRoutine implements IMorningRoutine {
   whatShouldIDoNow = () => {
+    if (this.getCurrentDate() === 6) return "Do exercise"
     return "No activity"
+  }
+
+  private getCurrentDate = () => {
+    return new Date().getHours()
   }
 }
